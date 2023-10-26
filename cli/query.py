deb7ad554870b9db2c16a86f42924b7df75b5f3f
lon-b <lon.baecker@dci-student.org>
2023-10-26 15:41:11 +0200

    finished milestone 4, basic commandline interface order system

--- cli/query.py
+++ cli/query.py
@@ -33,32 +33,62 @@ if chosen_option == "1":
     print("Items in Warehouse 2: ", *warehouse2, sep="\n- ")
     print("")
     print(f"Thank you for your visit, {username}!")
+
 elif chosen_option == "2":
-    search_query = input("What is the name of the item? name must be exact! ")
-    total_count = warehouse1.count(search_query) + warehouse2.count(search_query)
-    count_warehouse1 = warehouse1.count(search_query)
-    count_warehouse2 = warehouse2.count(search_query)
+    search_query = input("What is the name of the item? name must be exact! ").lower()
+
+    # search results will now be displayed in lower case :( not sure how to fix it
+    warehouse1_lower = [item.lower() for item in warehouse1]
+    warehouse2_lower = [item.lower() for item in warehouse2]
+
+    total_count = warehouse1_lower.count(search_query) + warehouse2_lower.count(search_query)
+    count_warehouse1 = warehouse1_lower.count(search_query)
+    count_warehouse2 = warehouse2_lower.count(search_query)
 
     if count_warehouse1 > 0 or count_warehouse2 > 0:
         print(f"Warehouse 1 has {count_warehouse1} {search_query}(s), Warehouse 2 has {count_warehouse2} {search_query}(s). Total: {total_count}")
     else:
         print("Not in stock.")
+
+    if count_warehouse1 > count_warehouse2:
+        print(f"Warehouse 1 has the highest amount of {search_query}(s): {count_warehouse1}")
+    elif count_warehouse2 > count_warehouse1:
+        print(f"Warehouse 2 has the highest amount of {search_query}(s): {count_warehouse2}")
+    else:
+        print("Error, restart.")
+    
+    if total_count >= 1:
+        interest_query = input("Would you like to place an order? (yes/no): ")
+        
+        if interest_query == "yes":
+            order_query = int(input("How many would you like to order? "))
+        
+            if order_query <= total_count:
+                print(f"Your order of {order_query} {search_query}(s) has been placed!")
+            
+            elif order_query > total_count:
+                print("Error, desired amount is higher than available stock!")
+                out_of_stock_query = input(f"Would you like to order the maximum available stock of {total_count} {search_query}? (yes/no): ")
+                
+                if out_of_stock_query == "yes":
+                    print(f"Your order of {total_count} {search_query}(s) has been placed!")
+                else:
+                    print(f"Thank you for your visit, {username}!")
+
+        else:
+            print(f"Thank you for your visit, {username}!")
+    
+    else:
+        print(f"Thank you for your visit, {username}!")
+
+
 elif chosen_option == "3":
-    print("chose 3")
+    pass
 else:
-    input("Problem; please choose an option from the list by typing 1, 2 or 3 and push the enter key: ") 
+    print("Error, invalid entry. Please try again!")
+    
+    # input("Problem; please choose an option from the list by typing 1, 2 or 3 and push the enter key: ") 
     #need to make it loop. do I just give this a function and make it recursive?
 
 
 # the items in the warehouse are written once or twice in order to simulate the amount of items the warehouse has in stock.
-
-# If they pick 1
-#
-# Else, if they pick 2
-#
-# Else, if they pick 3
-#
-# Else
-
-# Thank the user for the visit
-
