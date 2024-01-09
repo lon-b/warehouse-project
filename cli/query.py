636fd481372b29d1235f65832a3c0e2a7e078b14
Lon Bäcker <don@Lons-Mac.fritz.box>
2024-01-09 11:19:46 +0100

    checking git connection on device

--- cli/query.py
+++ cli/query.py
@@ -1,4 +1,4 @@
-from data import warehouse1, warehouse2
+from data import stock
 
 # Get the user name
 username = input("What is your user name? ")
@@ -15,23 +15,75 @@ for option in options:
 
 chosen_option = input("Type the number of the option you'd like to choose: ")
 
-if chosen_option == "1":
-    print("Items in Warehouse 1: ", *warehouse1, sep="\n- ")
-    print("")
-    print("Items in Warehouse 2: ", *warehouse2, sep="\n- ")
-    print("")
+
+def clean_wh1_stock():
+    warehouse1_items_list = []
+    for item in stock:
+        if item["warehouse"] == 1:
+            warehouse1_items_list.append(item)
+    return warehouse1_items_list
+
+
+def print_wh1_stock():
+    warehouse1_items_list = clean_wh1_stock()
+    for item in warehouse1_items_list:
+        print("- " + item["state"] + " " + item["category"])
+
+
+def clean_wh2_stock():
+    warehouse2_items_list = []
+    for item in stock:
+        if item["warehouse"] == 2:
+            warehouse2_items_list.append(item)
+    return warehouse2_items_list
+
+
+def print_wh2_stock():
+    warehouse2_items_list = clean_wh2_stock()
+    for item in warehouse2_items_list:
+        print("- " + item["state"] + " " + item["category"])
+
+
+def count_wh1_stock():
+    wh1_items = clean_wh1_stock()
+    warehouse1_count = len(wh1_items)
+    return warehouse1_count
+
+
+def count_wh2_stock():
+    wh2_items = clean_wh2_stock()
+    warehouse2_count = len(wh2_items)
+    return warehouse2_count
+
+
+def count_total_stock():
+    total_count_stock = count_wh1_stock() + count_wh2_stock()
+    return total_count_stock
+
+
+if chosen_option == "1":  # option 1
+    print("Items in Warehouse 1: ")
+    print_wh1_stock()
+    print(" ")
+    print("Items in Warehouse 2: ")
+    print_wh2_stock()
+    print(" ")
+    print(f"Total items in Warehouse 1: {count_wh1_stock()}")
+    print(" ")
+    print(f"Total items in Warehouse 2: {count_wh2_stock()}")
+    print(" ")
     print(f"Thank you for your visit, {username}!")
 
-elif chosen_option == "2":
-    search_query = input("What is the name of the item? name must be exact! ").lower()
 
-    # search results will now be displayed in lower case :( not sure how to fix it
-    warehouse1_lower = [item.lower() for item in warehouse1]
-    warehouse2_lower = [item.lower() for item in warehouse2]
+elif chosen_option == "2":  # option 2
+    search_query = input("What is the name of the item you want to search for? ").lower()
 
-    total_count = warehouse1_lower.count(search_query) + warehouse2_lower.count(search_query)
-    count_warehouse1 = warehouse1_lower.count(search_query)
-    count_warehouse2 = warehouse2_lower.count(search_query)
+    warehouse1_items = clean_wh1_stock()
+    warehouse2_items = clean_wh2_stock()
+
+    total_count = warehouse1_items.count(search_query) + warehouse2_items.count(search_query)
+    count_warehouse1 = warehouse1_items.count(search_query)
+    count_warehouse2 = warehouse2_items.count(search_query)
 
     if count_warehouse1 > 0 or count_warehouse2 > 0:
         print(f"Warehouse 1 has {count_warehouse1} {search_query}(s), Warehouse 2 has {count_warehouse2} {search_query}(s). Total: {total_count}")
@@ -44,39 +96,31 @@ elif chosen_option == "2":
         print(f"Warehouse 2 has the highest amount of {search_query}(s): {count_warehouse2}")
     else:
         print("Error, restart.")
-    
+
     if total_count >= 1:
         interest_query = input("Would you like to place an order? (yes/no): ")
-        
+
         if interest_query == "yes":
             order_query = int(input("How many would you like to order? "))
-        
-            if order_query <= total_count:
-                print(f"Your order of {order_query} {search_query}(s) has been placed!")
-            
-            elif order_query > total_count:
-                print("Error, desired amount is higher than available stock!")
-                out_of_stock_query = input(f"Would you like to order the maximum available stock of {total_count} {search_query}? (yes/no): ")
-                
-                if out_of_stock_query == "yes":
-                    print(f"Your order of {total_count} {search_query}(s) has been placed!")
-                else:
-                    print(f"Thank you for your visit, {username}!")
+            out_of_stock_query = input(f"We have {total_count} {search_query}(s) in stock. Is that enough? (yes/no): ")
 
+            if out_of_stock_query == "yes":
+                print(f"Your order of {total_count} {search_query}(s) has been placed!")
+            else:
+                print(f"Thank you for your visit, {username}!")
         else:
             print(f"Thank you for your visit, {username}!")
-    
     else:
         print(f"Thank you for your visit, {username}!")
 
 
 elif chosen_option == "3":
-    pass
+    print(f"Thank you for your visit, {username}!")
 else:
     print("Error, invalid entry. Please try again!")
-    
-    # input("Problem; please choose an option from the list by typing 1, 2 or 3 and push the enter key: ") 
-    #need to make it loop. do I just give this a function and make it recursive?
+
+    # input("Problem; please choose an option from the list by typing 1, 2 or 3 and push the enter key: ")
+    # need to make it loop. do I just give this a function and make it recursive?
 
 
 # the items in the warehouse are written once or twice in order to simulate the amount of items the warehouse has in stock.
